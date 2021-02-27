2cfd9aa2e48f7fe0be2c184363c7c53f8e32875d
Adah Eni <eniwokepdm@gmail.com>
2021-02-27 18:08:12 +0000

    refactor: get HubData

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from utils import HubData
 import argparse
 
+
 def get_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--period',
-                        choices=['day', 'week', 'month'],
-                        default='day'
-                       )
+    parser.add_argument("--period", choices=["day", "week", "month"], default="day")
 
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     args = get_args()
-    period = args.get("period")
-    print(period)
-    # HubData(period)
+    period = args.period
+    hub_data = HubData(period)
