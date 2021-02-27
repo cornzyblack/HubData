954426ef446082117b6acf84371bca06b118e252
Adah Eni <eniwokepdm@gmail.com>
2021-02-27 18:28:06 +0000

    refactor: change process of storing file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_args():
     parser.add_argument(
         "--period", choices=["day", "week", "month", "year"], default="day"
     )
-
+    parser.add_argument("--filename", default=None)
     return parser.parse_args()
 
 
@@ -16,3 +16,5 @@ if __name__ == "__main__":
     args = get_args()
     period = args.period
     hub_data = HubData(period)
+
+    hub_data.save_to_csv(args.filename)
