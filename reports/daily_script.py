b9287e74bc05ddf8db330a30f6447952e6fada02
Adah Eni <eniwokepdm@gmail.com>
2021-02-28 04:59:42 +0000

    chore: add reports

---
+++ reports/daily_script.py
@@ -0,0 +1,35 @@
+import altair as alt
+import pandas as pd
+import datapane as dp
+from datetime import date
+from dotenv import load_env
+
+load_env()
+# Sign-in with your unique token
+
+api_token = os.getenv("token")
+dp.login(token=api_token)
+
+df = pd.read_csv("data/daily_prices.csv")
+
+plot = (
+    alt.Chart(df)
+    .mark_line()
+    .encode(x="Date", y="Price ($)")
+    .interactive()
+    .properties(width="container")
+)
+
+# Create report
+r = dp.Report(
+    f"### Line plot Showing Daily Oil Prices World in Data](http://www.eia.gov/naturalgas) on {date.today()}_",
+    dp.Plot(plot),
+    dp.DataTable(df),
+)
+
+# Publish
+r.publish(
+    name=f"Daily Oil Prices World in Data",
+    open=True,
+    description=f"Daily Oil Prices from Hub Data",
+)
